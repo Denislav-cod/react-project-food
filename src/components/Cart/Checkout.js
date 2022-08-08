ca4134f9ef3ffabf3052d8007358c482ba621f60
Denislav-cod <69403505+Denislav-cod@users.noreply.github.com>
2022-08-08 15:59:59 +0300

    Checkout.js update
    use-form-hook has been replaced with use state and now send one nested object to the firebase

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,56 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from "axios";
-import { useForm } from 'react-hook-form';
+
 import classes from './Checkout.module.css';
 import Modal from '../UI/Modal';
 
 const Checkout = (props) => {
-    const { register, handleSubmit, resetField } = useForm();
-    const confirmHandler = async (data) => {
-        const completeOrder = {
-            order: {
-                order: props.order,
-                totalAmount: props.totalAmount,
-                orderInfo: data
-            },
+    const [name, setName] = useState();
+    const [street, setStreet] = useState();
+    const [postal, setPostal] = useState();
+    const [city, setCity] = useState();
+
+    const submitData = async (event) => {
+        event.preventDefault();
+        const order = {
+            orderInfo: props.order,
             status: "pending",
-
-        }
-        await axios.post("https://react-foodsite-default-rtdb.europe-west1.firebasedatabase.app/orders.json", { order: completeOrder });
-        resetField('name');
-        resetField('postal');
-        resetField('street');
-        resetField('city');
+            userInfo: {
+                name: name,
+                street: street,
+                postal: postal,
+                city: city
+            },
+            
+        };
+        const response = await axios.post("https://react-foodsite-default-rtdb.europe-west1.firebasedatabase.app/orders.json", order)
+            .catch((error) => {
+                console.log(error);
+            })
     }
 
-    const handleCheckout = () => {
-        props.onHandleCheckout();
-    }
+    const nameChangeHandler = (event) => {
+        setName(event.target.value);
+    };
+    const streetChangeHandler = (event) => {
+        setStreet(event.target.value);
+    };
+    const postalChangeHandler = (event) => {
+        setPostal(event.target.value);
+    };
+    const cityChangeHandler = (event) => {
+        setCity(event.target.value);
+    };
+
 
     return (
-        <Modal onClick={handleCheckout}>
-            <form className={classes.form} onSubmit={handleSubmit(confirmHandler)}>
+        <Modal>
+            <form className={classes.form} onSubmit={submitData}>
                 <div className={classes.control}>
                     <label htmlFor='name'>Your Name</label>
-                    <input type='text' id='name' {...register("name", { required: true })} />
+                    <input type='text' id='name' onChange={nameChangeHandler} />
                 </div>
                 <div className={classes.control}>
                     <label htmlFor='street'>Street</label>
-                    <input type='text' id='street' {...register("street", { required: true })} />
+                    <input type='text' id='street' onChange={streetChangeHandler} />
                 </div>
                 <div className={classes.control}>
                     <label htmlFor='postal'>Postal Code</label>
-                    <input type='text' id='postal' {...register("postal", { required: true })} />
+                    <input type='text' id='postal' onChange={postalChangeHandler} />
                 </div>
                 <div className={classes.control}>
                     <label htmlFor='city'>City</label>
-                    <input type='text' id='city'  {...register("city", { required: true })} />
+                    <input type='text' id='city' onChange={cityChangeHandler} />
                 </div>
                 <div className={classes.actions}>
                     <button type='button' onClick={props.onCancel}>
                         Cancel
                     </button>
-                    <button  className={classes.submit}>Confirm</button>
+                    <button className={classes.submit}>Confirm</button>
                 </div>
             </form>
         </Modal>
