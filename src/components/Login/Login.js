30a693e626d1eb2ff64057b473c455880187e525
Denislav-cod <69403505+Denislav-cod@users.noreply.github.com>
2022-09-16 12:33:47 +0300

    Added login and registration logic
    need to be improved

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,18 +1,48 @@
+import { useState, useRef } from "react";
+import axios from "axios";
 import Card from "../UI/Card";
-import classes from "./Login.module.css"
+import classes from "./Login.module.css";
 
 const Login = () => {
+    const [error, setError] = useState();
+    const [isError, setIsError] = useState(false);
+    const nameRef = useRef();
+    const passwordRef = useRef();
+
+    const submitHandler = async (event) => {
+        event.preventDefault();
+
+        const enteredName = nameRef.current.value;
+        const enteredPassword = passwordRef.current.value;
+
+        //Later add validation and maybe create one auth file with button for change registration or login;
+        try {
+            const response = await axios.post("http://localhost:3001/users/login",
+                {
+                    name: enteredName,
+                    password: enteredPassword
+                }
+            );
+            console.log(response);
+        } catch (error) {
+            setError(error.message)
+            setIsError(true);
+        }
+
+    }
+
     return (
         <Card className={classes.login}>
             <section >
-                <form>
+                {isError === true && <p>{error}</p>}
+                <form onSubmit={submitHandler}>
                     <div className={classes.control}>
                         <label htmlFor='name'> Name</label>
-                        <input type='text' id='name' />
+                        <input type='text' id='name' required ref={nameRef} />
                     </div>
                     <div className={classes.control}>
                         <label htmlFor='password'>Password</label>
-                        <input type='password' id='password' />
+                        <input type='password' id='password' required ref={passwordRef} />
                     </div>
                     <div>
                         <button className={classes.button}>Login</button>
--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,22 +1,51 @@
-import classes from "./Registration.module.css";
+import { useRef } from "react";
+import axios from "axios";
 import Card from "../UI/Card";
+import classes from "./Registration.module.css";
 
 const Registration = () => {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    const submitHandler = async (event) => {
+        event.preventDefault();
+
+        const enteredName = nameRef.current.value;
+        const enteredEmail = emailRef.current.value;
+        const enteredPassword = passwordRef.current.value;
+
+        //Later add validation same as login comment;
+        try {
+            const response = await axios.post("http://localhost:3001/users/registration",
+                {
+                    name: enteredName,
+                    email: enteredEmail,
+                    password: enteredPassword
+                }
+            );
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
     return (
         <Card className={classes.regster}>
             <section >
-                <form>
+                <form onSubmit={submitHandler}>
                     <div className={classes.control}>
                         <label htmlFor='name'>Name</label>
-                        <input type='text' id='name' />
+                        <input type='text' id='name' ref={nameRef} />
                     </div>
                     <div className={classes.control}>
                         <label htmlFor='email'>Email</label>
-                        <input type='text' id='email' />
+                        <input type='text' id='email' ref={emailRef} />
                     </div>
                     <div className={classes.control}>
                         <label htmlFor='password'>Password</label>
-                        <input type='password' id='password' />
+                        <input type='password' id='password' ref={passwordRef} />
                     </div>
                     <div>
                         <button className={classes.button}>Registration</button>
